3c57b9de58102ae8074a1faa8d977f05c9f87a38
Mohammad Khaled Anwar <mohammed.khaled.anwar@gmail.com>
2025-10-09 20:15:23 +0300

    Add files via upload

--- CS213_A1_Part1_S30_20240152_20240493_20240550.cpp
+++ CS213_A1_Part1_S30_20240152_20240493_20240550.cpp
@@ -1,3 +1,6 @@
+// Name: Mohamed Khaled Anwar
+// Id: 20240493
+
 #include <bits/stdc++.h>
 #include "Image_Class.h"
 
@@ -7,7 +10,7 @@ Image img;
 bool loaded = false;
 bool modified = false;
 
-
+// check valid extension
 bool validExtension(const string &name) {
     string exts[] = {".jpg", ".png", ".bmp", ".jpeg"};
     for (auto &e: exts) {
@@ -81,133 +84,187 @@ void saveImage() {
     }
 }
 
-void grayScale() {
+// Filter 2: Black and White
+void blackWhite() {
     if (!loaded) {
         cout << "Load an image first!\n";
         return;
     }
-    for (int i = 0; i < img.width; i++)
-        for (int j = 0; j < img.height; j++) {
-            unsigned avg = 0;
-            for (int k = 0; k < img.channels; k++)
+
+    for (int i = 0; i < img.width; ++i) {
+        for (int j = 0; j < img.height; ++j) {
+
+            unsigned int avg = 0;
+            for (int k = 0; k < img.channels; ++k) {
                 avg += img(i, j, k);
-            avg /= img.channels;
-            for (int k = 0; k < img.channels; k++)
-                img(i, j, k) = avg;
-        }
-    cout << "Gray Scale applied.\n";
-    modified = true;
-}
+            }
+            avg = avg / img.channels;
 
-void edgeDetection() {
-    if (!loaded) {
-        cout << "Load an image first!\n";
-        return;
-    }
-    for (int i = 0; i < img.width; i++) {
-        for (int j = 0; j < img.height; j++) {
-            unsigned avg = 0;
-            for (int k = 0; k < img.channels; k++)
-                avg += img(i, j, k);
-
-            avg /= 3;
-
-            unsigned bw = (avg < 128) ? 0 : 255;
-            for (int k = 0; k < img.channels; k++)
-                img(i, j, k) = bw;
-        }
-    }
-
-    Image result = img;
-    for (int i = 1; i < img.width; i++) {
-        for (int j = 1; j < img.height; j++) {
-            int current = img(i, j, 0);
-            int prev = img(i - 1, j, 0);
-            int up = img(i, j - 1, 0);
-
-            if (current == prev && current == up)
-                result(i, j, 0) = result(i, j, 1) = result(i, j, 2) = 255; // أبيض
-            else
-                result(i, j, 0) = result(i, j, 1) = result(i, j, 2) = 0;   // أسود
-        }
-    }
-
-    img = result;
-    cout << "Edge detection applied\n";
-    modified = true;
-}
-
-void adjustBrightness() {
-    if (!loaded) {
-        cout << "Load an image first!\n";
-        return;
-    }
-
-    int percent;
-    cout << "Enter brightness level (-100 to 100): ";
-    cin >> percent;
-
-    if (percent < -100 || percent > 100) {
-        cout << "Invalid value! Must be between -100 and 100.\n";
-        return;
-    }
-
-    float factor = percent / 100.0f;
-
-    for (int i = 0; i < img.width; i++) {
-        for (int j = 0; j < img.height; j++) {
-            for (int k = 0; k < img.channels; k++) {
-                int newVal = img(i, j, k) + img(i, j, k) * factor;
-                if (newVal > 255) newVal = 255;
-                if (newVal < 0) newVal = 0;
-                img(i, j, k) = newVal;
+            for (int k = 0; k < img.channels; ++k) {
+                if (avg < 128)
+                    img(i, j, k) = 0;
+                else
+                    img(i, j, k) = 255;
             }
         }
     }
 
-    if (percent > 0)
-        cout << "Image lightened by " << percent << "%.\n";
-    else if (percent < 0)
-        cout << "Image darkened by " << -percent << "%.\n";
-    else
-        cout << "No change applied.\n";
-
+    cout << "Black & White applied.\n";
     modified = true;
 }
 
-void merge() {
-    string n1, n2;
-    cout << "Enter first image: ";
-    cin >> n1;
-    cout << "Enter second image: ";
-    cin >> n2;
-    Image img1, img2;
-    if (!img1.loadNewImage(n1) || !img2.loadNewImage(n2)) {
-        cout << "Failed to load images!\n";
+// Filter 5: Flip Image
+void flip() {
+    if (!loaded) {
+        cout << "Load an image first!\n";
         return;
     }
-    int newW = max(img1.width, img2.width);
-    int newH = max(img1.height, img2.height);
-    Image r1(newW, newH), r2(newW, newH);
-    for (int i = 0; i < newW; i++)
-        for (int j = 0; j < newH; j++) {
-            int oldx1 = i * img1.width / newW;
-            int oldy1 = j * img1.height / newH;
-            int oldx2 = i * img2.width / newW;
-            int oldy2 = j * img2.height / newH;
-            for (int k = 0; k < img1.channels; k++) {
-                r1(i, j, k) = img1(oldx1, oldy1, k);
-                r2(i, j, k) = img2(oldx2, oldy2, k);
+    char your_choice;
+    cout << "Flip (H/V): ";
+    cin >> your_choice;
+
+    if (your_choice == 'H' || your_choice == 'h') {
+        for (int i = 0; i < img.width / 2; ++i) {
+            int i2 = img.width - 1 - i;
+            for (int j = 0; j < img.height; ++j) {
+                for (int k = 0; k < img.channels; ++k) {
+                    int tmp = img(i, j, k);
+                    img(i, j, k) = img(i2, j, k);
+                    img(i2, j, k) = tmp;
+                }
             }
         }
-    img = Image(newW, newH);
-    for (int i = 0; i < newW; i++)
-        for (int j = 0; j < newH; j++)
-            for (int k = 0; k < r1.channels; k++)
-                img(i, j, k) = (r1(i, j, k) + r2(i, j, k)) / 2;
+        cout << "Flipped Horizontally.\n";
+    } else {
+        for (int i = 0; i < img.width; ++i) {
+            for (int j = 0; j < img.height / 2; ++j) {
+                int j2 = img.height - 1 - j;
+                for (int k = 0; k < img.channels; ++k) {
+                    int tmp = img(i, j, k);
+                    img(i, j, k) = img(i, j2, k);
+                    img(i, j2, k) = tmp;
+                }
+            }
+        }
+        cout << "Flipped Vertically.\n";
+    }
+    modified = true;
+}
+
+// Filter 8: Crop Images
+void crop() {
+    if (!loaded) {
+        cout << "Load an image first!\n";
+        return;
+    }
+
+    cout << "Original image size: " << img.width << " x " << img.height << endl;
+
+    int x, y, w, h;
+    cout << "Enter the starting point (x y) of the upper-left corner:\n";
+    cin >> x >> y;
+    cout << "Enter the width and height (W H) of the area to crop:\n";
+    cin >> w >> h;
+
+    if (x < 0 || y < 0 || w <= 0 || h <= 0 || x + w > img.width || y + h > img.height) {
+        cout << "Invalid crop dimensions. They must fit within the original image.\n";
+        return;
+    }
+
+    Image cropped(w, h);
+
+    for (int j = 0; j < h; ++j) {
+        for (int i = 0; i < w; ++i) {
+            for (int c = 0; c < img.channels; ++c) {
+                cropped(i, j, c) = img(x + i, y + j, c);
+            }
+        }
+    }
+
+        img = cropped;
+        loaded = true;
+        cout << "Image cropped successfully.\n";
+        cout << "New image size: " << w << " x " << h << endl;
+        modified = true;
+
+}
+
+// Filter 11: Resizing Images
+void resizeImage() {
+    if (!loaded) {
+        cout << "Load an image first!\n";
+        return;
+    }
+
+    int newWidth, newHeight;
+    cout << "Enter the new width and height of the image:\n";
+    cin >> newWidth >> newHeight;
+
+    if (newWidth <= 0 || newHeight <= 0) {
+        cout << "Invalid dimensions!\n";
+        return;
+    }
+
+    // Create a new image with the desired size
+    Image resizedImage(newWidth, newHeight);
+
+    // Compute scale factors
+    float x_ratio = static_cast<float>(img.width) / newWidth;
+    float y_ratio = static_cast<float>(img.height) / newHeight;
+
+    // Nearest Neighbor Interpolation
+    for (int y = 0; y < newHeight; ++y) {
+        for (int x = 0; x < newWidth; ++x) {
+            int srcX = static_cast<int>(x * x_ratio);
+            int srcY = static_cast<int>(y * y_ratio);
+
+            for (int c = 0; c < img.channels; ++c) {
+                resizedImage(x, y, c) = img(srcX, srcY, c);
+            }
+        }
+    }
+
+
+    img = resizedImage;
     loaded = true;
+    cout << "Image resized successfully!\n";
+    cout << "Original size: " << img.width << "x" << img.height << endl;
+    cout << "New size: " << newWidth << "x" << newHeight << endl;
     modified = true;
-    cout << "Images merged.\n";
+
+}
+
+// Filter 16: looking so purple at night
+void wanoPurple() {
+    if (!loaded) {
+        cout << "Load an image first!\n";
+        return;
+    }
+
+    if (img.width <= 0 || img.height <= 0 || img.channels <= 0) {
+        cout << "Invalid image properties!\n";
+        return;
+    }
+
+    for (int i = 0; i < img.width; ++i) {
+        for (int j = 0; j < img.height; ++j) {
+            int red = img(i, j, 0);
+            int green = img(i, j, 1);
+            int blue = img(i, j, 2);
+
+            int newRed = min(255, red + 60);
+            int newGreen = max(0, green - 20);
+            int newBlue = min(255, blue + 40);
+
+            img(i, j, 0) = (newRed + red) / 2;
+            img(i, j, 1) = newGreen;
+            img(i, j, 2) = (newBlue + blue) / 2;
+        }
+    }
+
+    cout << "Wano purple night filter applied successfully!\n";
+    modified = true;
+
 }
 
 int main() {
@@ -218,12 +275,14 @@ int main() {
     do {
         cout << "\n==== Menu ====\n";
         cout << "1. Load New Image\n";
-        cout << "2. Gray Scale\n";
-        cout << "3. Merge Two Images\n";
-        cout << "4. Darken/Lighten Image\n";
-        cout << "5. Detect Edges\n";
-        cout << "6. Save Image\n";
-        cout << "7. Exit\n";
+        cout << "2. Black & White\n";
+        cout << "3. Flip\n";
+        cout << "4. Crop\n";
+        cout << "5. Resize\n";
+        cout << "6. Wano Purple Night\n";
+        cout << "7. Save Image\n";
+        cout << "8. Exit\n";
+        cout << "Choose option: ";
         cin >> choice;
 
         switch (choice) {
@@ -231,21 +290,24 @@ int main() {
                 loadImage();
                 break;
             case 2:
-                grayScale();
+                blackWhite();
                 break;
             case 3:
-                merge();
+                flip();
                 break;
             case 4:
-                adjustBrightness();
+                crop();
                 break;
-            case 5 :
-                edgeDetection();
+            case 5:
+                resizeImage();
                 break;
             case 6:
-                saveImage();
+                wanoPurple();
                 break;
             case 7:
+                saveImage();
+                break;
+            case 8:
                 if (!askToSave())
                     break;
                 cout << "Exiting...\n";
@@ -253,8 +315,7 @@ int main() {
             default:
                 cout << "Invalid choice!\n";
         }
-
-    } while (choice != 7);
+    } while (choice != 8);
 
     return 0;
 }
